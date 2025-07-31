312dd1294b6f018f6cbf19f66e58566d2c225323
SyedAbdullahAhmad <ahmedsyedabdullah674@gmail.com>
2025-07-31 18:02:01 +0500

    focusing input

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
+import { StrictMode,createContext } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const countingContext=createContext();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    
     <App />
+   
   </StrictMode>,
 )
+
+// export default countingContext;
